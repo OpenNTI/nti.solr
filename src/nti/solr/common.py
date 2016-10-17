9312dffce621a25454e1ae06e33bfa79dd50d519
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-17 20:59:24 +0000

    move func to common module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@98868 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/solr/common.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from datetime import datetime
+
+import isodate
+
+from nti.async import create_job
+
+from nti.solr import get_factory
+
+from nti.solr.interfaces import IIDValue
+
+def datetime_isoformat(now=None):
+	now = now or datetime.now()
+	return isodate.datetime_isoformat(now)
+
+def get_job_queue(name):
+	factory = get_factory()
+	return factory.get_queue(name)
+
+def add_2_queue(name, obj):
+	pass
+
+def queue_add(name, func, obj, **kwargs):
+	adpated = IIDValue(obj, None)
+	doc_id = adpated.value() if adpated is not None else None
+	if doc_id:
+		queue = get_job_queue(name)
+		job = create_job(func, obj, **kwargs)
+		job.id = '%s_%s_added' % (datetime_isoformat(), doc_id)
+		queue.put(job)
+
+def queue_modified(name, obj):
+	pass
+
+def queue_remove(name, func, obj):
+	pass
--- src/nti/solr/subscribers.py
+++ src/nti/solr/subscribers.py
@@ -16,35 +16,18 @@ from zope.intid.interfaces import IIntIdRemovedEvent
 
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
-from nti.async import create_job
-
 from nti.dataserver.interfaces import IUserGeneratedData
 from nti.dataserver.interfaces import IDeletedObjectPlaceholder
 
-from nti.solr import get_factory
 from nti.solr import USER_DATA_QUEUE
 
-def get_job_queue(name):
-	factory = get_factory()
-	return factory.get_queue(name)
-
-def add_2_queue(name, obj):
-	pass
-
-def queue_added(name, obj, **kwargs):
-	queue = get_job_queue(name)
-	job = create_job(None, obj, **kwargs)
-	queue.put(job)
-
-def queue_modified(name, obj):
-	pass
-
-def queue_remove(name, obj):
-	pass
+from nti.solr.common import queue_add
+from nti.solr.common import queue_remove
+from nti.solr.common import queue_modified
 
 @component.adapter(IUserGeneratedData, IIntIdAddedEvent)
 def _user_data_added(obj, event):
-	queue_added(USER_DATA_QUEUE, obj)
+	queue_add(USER_DATA_QUEUE, None, obj)
 
 @component.adapter(IUserGeneratedData, IObjectModifiedEvent)
 def _user_data_modified(obj, event):
