ae874960115679667954ef2f1cf86be8d572ea7b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-17 18:56:29 +0000

    work towards job queues
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@98856 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/subscribers.py
+++ src/nti/solr/subscribers.py
@@ -16,42 +16,43 @@ from zope.intid.interfaces import IIntIdRemovedEvent
 
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
+from nti.async import create_job
+
 from nti.dataserver.interfaces import IUserGeneratedData
 from nti.dataserver.interfaces import IDeletedObjectPlaceholder
 
 from nti.solr import get_factory
+from nti.solr import USER_DATA_QUEUE
 
 def get_job_queue(name):
-    factory = get_factory()
-    return factory.get_queue(name)
+	factory = get_factory()
+	return factory.get_queue(name)
 
-def add_2_queue(queue_name, obj):
-    pass
+def add_2_queue(name, obj):
+	pass
 
-def queue_added(obj):
-    try:
-        return add_2_queue(obj)
-    except TypeError:
-        pass
-    return False
+def queue_added(name, obj, **kwargs):
+	queue = get_job_queue(name)
+	job = create_job(None, obj, **kwargs)
+	queue.put(job)
 
-def queue_modified(obj):
-    pass
+def queue_modified(name, obj):
+	pass
 
-def queue_remove(obj):
-    pass
+def queue_remove(name, obj):
+	pass
 
 @component.adapter(IUserGeneratedData, IIntIdAddedEvent)
-def _user_data_added(modeled, event):
-    queue_added(modeled)
+def _user_data_added(obj, event):
+	queue_added(USER_DATA_QUEUE, obj)
 
 @component.adapter(IUserGeneratedData, IObjectModifiedEvent)
 def _user_data_modified(obj, event):
-    if IDeletedObjectPlaceholder.providedBy(obj):
-        queue_remove(obj)
-    else:
-        queue_modified(obj)
+	if IDeletedObjectPlaceholder.providedBy(obj):
+		queue_remove(USER_DATA_QUEUE, obj)
+	else:
+		queue_modified(USER_DATA_QUEUE, obj)
 
 @component.adapter(IUserGeneratedData, IIntIdRemovedEvent)
 def _user_data_removed(obj, event):
-    queue_remove(obj)
+	queue_remove(USER_DATA_QUEUE, obj)
