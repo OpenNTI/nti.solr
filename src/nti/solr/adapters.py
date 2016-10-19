18d0070d10fcded2c07755b2d06f960e86841b3f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-19 20:50:19 +0000

    remove dead code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@99032 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/adapters.py
+++ src/nti/solr/adapters.py
@@ -14,12 +14,7 @@ from zope import interface
 
 from nti.common.string import to_unicode
 
-from nti.dataserver.interfaces import IUserGeneratedData
-
-from nti.solr.interfaces import ICoreCatalog 
 from nti.solr.interfaces import IStringValue 
-
-from nti.solr.catalog import CoreCatalog
 
 @component.adapter(basestring)
 @interface.implementer(IStringValue)
@@ -34,8 +29,3 @@ class _StringValue(object):
 	def value(self, context=None):
 		context = self.context if context is None else context
 		return to_unicode(context) if context else None
-
-@interface.implementer(ICoreCatalog)
-@component.adapter(IUserGeneratedData)
-def _UserDataCatalog(obj):
-	return CoreCatalog('user_data')
