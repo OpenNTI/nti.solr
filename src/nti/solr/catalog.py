7ec7aa303583b3201297b5fa730baa087471064d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-27 14:40:29 +0000

    remove invalid import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@99462 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/catalog.py
+++ src/nti/solr/catalog.py
@@ -5,7 +5,6 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from prompt_toolkit.shortcuts import clear
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
