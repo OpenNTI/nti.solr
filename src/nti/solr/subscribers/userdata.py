a2705b766a0e9f7cf0e51149b030ddf47efc54bd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-25 17:50:59 +0000

    move userdata subs to its own module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@101361 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/solr/subscribers/userdata.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+
+from zope.intid.interfaces import IIntIdAddedEvent
+from zope.intid.interfaces import IIntIdRemovedEvent
+
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+
+from nti.dataserver.interfaces import IUserGeneratedData
+from nti.dataserver.interfaces import IDeletedObjectPlaceholder
+
+from nti.solr import USERDATA_QUEUE
+
+from nti.solr.interfaces import IIndexObjectEvent
+from nti.solr.interfaces import IUnindexObjectEvent
+
+from nti.solr.common import queue_add
+from nti.solr.common import queue_remove
+from nti.solr.common import queue_modified
+from nti.solr.common import single_index_job
+from nti.solr.common import single_unindex_job
+
+@component.adapter(IUserGeneratedData, IIntIdAddedEvent)
+def _userdata_added(obj, event):
+	queue_add(USERDATA_QUEUE, single_index_job, obj)
+
+@component.adapter(IUserGeneratedData, IObjectModifiedEvent)
+def _userdata_modified(obj, event):
+	if IDeletedObjectPlaceholder.providedBy(obj):
+		queue_remove(USERDATA_QUEUE, single_unindex_job, obj)
+	else:
+		queue_modified(USERDATA_QUEUE, single_index_job, obj)
+
+@component.adapter(IUserGeneratedData, IIntIdRemovedEvent)
+def _userdata_removed(obj, event):
+	queue_remove(USERDATA_QUEUE, single_unindex_job, obj=obj)
+
+@component.adapter(IUserGeneratedData, IIndexObjectEvent)
+def _index_userdata(obj, event):
+	queue_add(obj, None)
+
+@component.adapter(IUserGeneratedData, IUnindexObjectEvent)
+def _unindex_userdata(obj, event):
+	queue_remove(obj, None)
