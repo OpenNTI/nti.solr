315bb2341228ae2b1e5322936bd27559da4a1e06
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-04 22:50:50 +0000

    fix typo in comment; pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@103672 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/utils.py
+++ src/nti/solr/utils.py
@@ -56,138 +56,153 @@ from nti.solr.termextract import extract_key_words as term_extract_key_words
 
 # content
 
+
 def resolve_content_parts(data):
-	result = []
-	data = [data] if isinstance(data, six.string_types) else data
-	for item in data or ():
-		adapted = IStringValue(item, None)
-		if adapted is not None:
-			result.append(adapted.value())
-	result = u'\n'.join(x for x in result if x is not None)
-	return result
+    result = []
+    data = [data] if isinstance(data, six.string_types) else data
+    for item in data or ():
+        adapted = IStringValue(item, None)
+        if adapted is not None:
+            result.append(adapted.value())
+    result = u'\n'.join(x for x in result if x is not None)
+    return result
+
 
 def get_content(text=None, lang="en"):
-	if not text or not isinstance(text, six.string_types):
-		result = u''
-	else:
-		text = to_unicode(text)
-		result = tokenize_content(text, lang)
-		result = ' '.join(result) if result else text
-	return result
+    if not text or not isinstance(text, six.string_types):
+        result = u''
+    else:
+        text = to_unicode(text)
+        result = tokenize_content(text, lang)
+        result = ' '.join(result) if result else text
+    return result
+
 
 def get_keywords(content, lang='en'):
-	keywords = extract_key_words(content, lang='en')
-	if not keywords:
-		keywords = term_extract_key_words(content, lang=lang)
-	return keywords
+    keywords = extract_key_words(content, lang='en')
+    if not keywords:
+        keywords = term_extract_key_words(content, lang=lang)
+    return keywords
 
 # documents
 
-def document_creator(obj, factory, provided=None):
-	result = factory()
-	if provided is None:
-		provided = find_most_derived_interface(result, ICoreDocument)
-	for k, v in provided.namesAndDescriptions(all=True):
-		__traceback_info__ = k, v
-		if IMethod.providedBy(v):
-			continue
-		value_interface = v.queryTaggedValue('__solr_value_interface__')
-		if value_interface is None:
-			continue
-		adapted = value_interface(obj, None)
-		if adapted is not None:
-			value = adapted.value()
-			setattr(result, k, value)
-	return result
 
-# pattern to get any prefix por post fix that catalog may add to the 
+def document_creator(obj, factory, provided=None):
+    result = factory()
+    if provided is None:
+        provided = find_most_derived_interface(result, ICoreDocument)
+    for k, v in provided.namesAndDescriptions(all=True):
+        __traceback_info__ = k, v
+        if IMethod.providedBy(v):
+            continue
+        value_interface = v.queryTaggedValue('__solr_value_interface__')
+        if value_interface is None:
+            continue
+        adapted = value_interface(obj, None)
+        if adapted is not None:
+            value = adapted.value()
+            setattr(result, k, value)
+    return result
+
+# pattern to get any prefix and/or post fix that a catalog may add to the
 # document ids. The prefix is anything that comes before the first '#' and
 # may be use as split.key for sharding. The postfix is anything after '@'
 # in this application ids CANNOT have an @
-_key_pattern = re.compile(r'([a-zA-Z0-9_.+-:,@]+\#)?([a-zA-Z0-9_.+-:,]+)(@.*)?$', 
-						  re.UNICODE | re.IGNORECASE)
+_key_pattern = re.compile(r'([a-zA-Z0-9_.+-:,@]+\#)?([a-zA-Z0-9_.+-:,]+)(@.*)?$',
+                          re.UNICODE | re.IGNORECASE)
+
+
 def normalize_key(doc_id):
-	m = _key_pattern.match(doc_id)
-	if m is not None:
-		return m.groups()[1]
-	return doc_id
+    m = _key_pattern.match(doc_id)
+    if m is not None:
+        return m.groups()[1]
+    return doc_id
+
 
 def object_finder(doc_id, intids=None):
-	doc_id = normalize_key(doc_id)
-	if is_valid_ntiid_string(doc_id):
-		return find_object_with_ntiid(doc_id)
-	else:
-		try:
-			intids = component.getUtility(IIntIds) if intids is None else intids
-			return intids.queryObject(int(doc_id))
-		except (ValueError, TypeError):
-			logger.error("Cannot get object with id %s", doc_id)
+    doc_id = normalize_key(doc_id)
+    if is_valid_ntiid_string(doc_id):
+        return find_object_with_ntiid(doc_id)
+    else:
+        try:
+            intids = component.getUtility(
+                IIntIds) if intids is None else intids
+            return intids.queryObject(int(doc_id))
+        except (ValueError, TypeError):
+            logger.error("Cannot get object with id %s", doc_id)
 
 _f_pattern = re.compile('(.*)(_[a-z]{2})$', re.UNICODE | re.IGNORECASE)
+
+
 def normalize_field(name):
-	m = _f_pattern.match(name)
-	if m is not None:
-		return m.groups()[0]
-	return name
+    m = _f_pattern.match(name)
+    if m is not None:
+        return m.groups()[0]
+    return name
 
 # searcher
 
+
 def transacted_func(func=None, **kwargs):
-	assert func is not None
+    assert func is not None
 
-	# prepare function call
-	new_callable = functools.partial(func, **kwargs)
+    # prepare function call
+    new_callable = functools.partial(func, **kwargs)
 
-	site_names = (getSite().__name__,)
+    site_names = (getSite().__name__,)
 
-	def _runner():
-		from nti.site.interfaces import ISiteTransactionRunner
-		transaction_runner = component.getUtility(ISiteTransactionRunner)
-		transaction_runner = functools.partial(transaction_runner,
-											   site_names=site_names,
-											   side_effect_free=True)
-		return transaction_runner(new_callable)
+    def _runner():
+        from nti.site.interfaces import ISiteTransactionRunner
+        transaction_runner = component.getUtility(ISiteTransactionRunner)
+        transaction_runner = functools.partial(transaction_runner,
+                                               site_names=site_names,
+                                               side_effect_free=True)
+        return transaction_runner(new_callable)
 
-	return _runner
+    return _runner
+
 
 def gevent_spawn(func=None, **kwargs):
-	greenlet = gevent.spawn(transacted_func(func, **kwargs))
-	return greenlet
+    greenlet = gevent.spawn(transacted_func(func, **kwargs))
+    return greenlet
 
 # MimeType / Catalog registry
-class MimeTypeRegistry(object):
-	
-	instance = None
-	
-	def __new__(cls, *args, **kwargs):
-		if cls.instance is None:
-			cls.instance = super(MimeTypeRegistry, cls).__new__(cls, *args, **kwargs)
-		return cls.instance
 
-	def __init__(self, *args, **kwargs):
-		self.mime_type_catalog = {}
-		self.catalog_mime_type = defaultdict(set)
-		
-	def register_mime_type(self, mimeType, catalog):
-		if mimeType in self.mime_type_catalog:
-			raise ValueError("mimeType already registered")
-		if catalog == USERDATA_CATALOG:
-			raise ValueError("Cannot register in userdata catalog")
-		self.mime_type_catalog[mimeType] = catalog
-		self.catalog_mime_type[catalog].add(mimeType)
-		self.catalog_mime_type[USERDATA_CATALOG].add(mimeType) # Negative list
-	registerMimeType = register_mime_type
-	
-	def get_catalog(self, mimeType, default=None):
-		return self.mime_type_catalog.get(mimeType, default)
-	getCatalog = get_catalog
-	
-	def get_mime_types(self, catalog, default=()):
-		result = self.catalog_mime_type.get(catalog)
-		return tuple(result) if result else default
-	getMimeTypes = get_mime_types
-	
-# Global registry 
+
+class MimeTypeRegistry(object):
+
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.instance is None:
+            cls.instance = super(MimeTypeRegistry, cls).__new__(
+                cls, *args, **kwargs)
+        return cls.instance
+
+    def __init__(self, *args, **kwargs):
+        self.mime_type_catalog = {}
+        self.catalog_mime_type = defaultdict(set)
+
+    def register_mime_type(self, mimeType, catalog):
+        if mimeType in self.mime_type_catalog:
+            raise ValueError("mimeType already registered")
+        if catalog == USERDATA_CATALOG:
+            raise ValueError("Cannot register in userdata catalog")
+        self.mime_type_catalog[mimeType] = catalog
+        self.catalog_mime_type[catalog].add(mimeType)
+        self.catalog_mime_type[USERDATA_CATALOG].add(mimeType)  # Negative list
+    registerMimeType = register_mime_type
+
+    def get_catalog(self, mimeType, default=None):
+        return self.mime_type_catalog.get(mimeType, default)
+    getCatalog = get_catalog
+
+    def get_mime_types(self, catalog, default=()):
+        result = self.catalog_mime_type.get(catalog)
+        return tuple(result) if result else default
+    getMimeTypes = get_mime_types
+
+# Global registry
 mimeTypeRegistry = MimeTypeRegistry()
 
 # Known mimeTypes used to map to their corresponding  search catalogs
@@ -209,42 +224,50 @@ COMMUNITY_MIME_TYPE = u'application/vnd.nextthought.community'
 DFL_MIME_TYPE = u'application/vnd.nextthought.dynamicfriendslist'
 FRIEND_LISTS_MIME_TYPE = u'application/vnd.nextthought.friendslist'
 
+
 def _register():
-	# content
-	mimeTypeRegistry.registerMimeType(CONTENT_MIME_TYPE, CONTENT_UNITS_CATALOG)
-	mimeTypeRegistry.registerMimeType(BOOK_CONTENT_MIME_TYPE, CONTENT_UNITS_CATALOG)
-	mimeTypeRegistry.registerMimeType(CONTENT_UNIT_MIME_TYPE, CONTENT_UNITS_CATALOG)
-	mimeTypeRegistry.registerMimeType(CONTENT_PACKAGE_MIME_TYPE, CONTENT_UNITS_CATALOG)
-	
-	# transcripts
-	mimeTypeRegistry.registerMimeType(NTI_TRANSCRIPT_MIME_TYPE, TRANSCRIPTS_CATALOG)
-	mimeTypeRegistry.registerMimeType(AUDIO_TRANSCRIPT_MIME_TYPE, TRANSCRIPTS_CATALOG)
-	mimeTypeRegistry.registerMimeType(VIDEO_TRANSCRIPT_MIME_TYPE, TRANSCRIPTS_CATALOG)
-	
-	# courses
-	mimeTypeRegistry.registerMimeType(COURSE_MIME_TYPE, COURSES_CATALOG)
-	mimeTypeRegistry.registerMimeType(CATALOG_ENTRY_MIME_TYPE, COURSES_CATALOG)
-	mimeTypeRegistry.registerMimeType(CATALOG_LEGACY_ENTRY_MIME_TYPE, COURSES_CATALOG)
-	
-	# entities
-	mimeTypeRegistry.registerMimeType(DFL_MIME_TYPE, ENTITIES_CATALOG)
-	mimeTypeRegistry.registerMimeType(USER_MIME_TYPE, ENTITIES_CATALOG)
-	mimeTypeRegistry.registerMimeType(COMMUNITY_MIME_TYPE, ENTITIES_CATALOG)
-	mimeTypeRegistry.registerMimeType(FRIEND_LISTS_MIME_TYPE, ENTITIES_CATALOG)
-	
-	# assets
-	for m in chain(AUDIO_MIMETYES, 
-				   VIDEO_MIMETYES, 
-				   TIMELINE_MIMETYES,
-				   RELATED_WORK_REF_MIMETYES):
-		mimeTypeRegistry.registerMimeType(m, ASSETS_CATALOG)
-		
-	# evaluations
-	try:
-		from nti.assessment.interfaces import ALL_EVALUATION_MIME_TYPES
-		for m in ALL_EVALUATION_MIME_TYPES:
-			mimeTypeRegistry.registerMimeType(m, EVALUATIONS_CATALOG)
-	except ImportError:
-		pass
+    # content
+    mimeTypeRegistry.registerMimeType(CONTENT_MIME_TYPE, CONTENT_UNITS_CATALOG)
+    mimeTypeRegistry.registerMimeType(
+        BOOK_CONTENT_MIME_TYPE, CONTENT_UNITS_CATALOG)
+    mimeTypeRegistry.registerMimeType(
+        CONTENT_UNIT_MIME_TYPE, CONTENT_UNITS_CATALOG)
+    mimeTypeRegistry.registerMimeType(
+        CONTENT_PACKAGE_MIME_TYPE, CONTENT_UNITS_CATALOG)
+
+    # transcripts
+    mimeTypeRegistry.registerMimeType(
+        NTI_TRANSCRIPT_MIME_TYPE, TRANSCRIPTS_CATALOG)
+    mimeTypeRegistry.registerMimeType(
+        AUDIO_TRANSCRIPT_MIME_TYPE, TRANSCRIPTS_CATALOG)
+    mimeTypeRegistry.registerMimeType(
+        VIDEO_TRANSCRIPT_MIME_TYPE, TRANSCRIPTS_CATALOG)
+
+    # courses
+    mimeTypeRegistry.registerMimeType(COURSE_MIME_TYPE, COURSES_CATALOG)
+    mimeTypeRegistry.registerMimeType(CATALOG_ENTRY_MIME_TYPE, COURSES_CATALOG)
+    mimeTypeRegistry.registerMimeType(
+        CATALOG_LEGACY_ENTRY_MIME_TYPE, COURSES_CATALOG)
+
+    # entities
+    mimeTypeRegistry.registerMimeType(DFL_MIME_TYPE, ENTITIES_CATALOG)
+    mimeTypeRegistry.registerMimeType(USER_MIME_TYPE, ENTITIES_CATALOG)
+    mimeTypeRegistry.registerMimeType(COMMUNITY_MIME_TYPE, ENTITIES_CATALOG)
+    mimeTypeRegistry.registerMimeType(FRIEND_LISTS_MIME_TYPE, ENTITIES_CATALOG)
+
+    # assets
+    for m in chain(AUDIO_MIMETYES,
+                   VIDEO_MIMETYES,
+                   TIMELINE_MIMETYES,
+                   RELATED_WORK_REF_MIMETYES):
+        mimeTypeRegistry.registerMimeType(m, ASSETS_CATALOG)
+
+    # evaluations
+    try:
+        from nti.assessment.interfaces import ALL_EVALUATION_MIME_TYPES
+        for m in ALL_EVALUATION_MIME_TYPES:
+            mimeTypeRegistry.registerMimeType(m, EVALUATIONS_CATALOG)
+    except ImportError:
+        pass
 _register()
 del _register
