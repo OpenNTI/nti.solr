dd5c247ce7aa8c3d20bd265c18dd9412844a0425
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-12 03:19:20 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@104143 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/subscribers/assets.py
+++ src/nti/solr/subscribers/assets.py
@@ -35,50 +35,58 @@ from nti.solr.common import queue_modified
 from nti.solr.common import single_index_job
 from nti.solr.common import single_unindex_job
 
+
 @component.adapter(INTITranscript, IIndexObjectEvent)
 def _index_transcript(obj, event):
-	queue_add(TRANSCRIPTS_QUEUE, single_index_job, obj)
+    queue_add(TRANSCRIPTS_QUEUE, single_index_job, obj)
+
 
 @component.adapter(INTITranscript, IUnindexObjectEvent)
 def _unindex_transcript(obj, event):
-	queue_add(TRANSCRIPTS_QUEUE, single_unindex_job, obj)
+    queue_add(TRANSCRIPTS_QUEUE, single_unindex_job, obj)
+
 
 @component.adapter(IPresentationAsset, IAfterIdAddedEvent)
 def _asset_added(obj, event=None):
-	if 		INTIMedia.providedBy(obj) \
-		or	INTIDocketAsset.providedBy(obj):
-		queue_add(ASSETS_QUEUE, single_index_job, obj)
-	if INTIMedia.providedBy(obj):
-		for transcript in getattr(obj, 'transcripts', None) or ():
-			_index_transcript(transcript, None)
+    if     INTIMedia.providedBy(obj) \
+        or INTIDocketAsset.providedBy(obj):
+        queue_add(ASSETS_QUEUE, single_index_job, obj)
+    if INTIMedia.providedBy(obj):
+        for transcript in getattr(obj, 'transcripts', None) or ():
+            _index_transcript(transcript, None)
+
 
 @component.adapter(IPresentationAsset, IObjectModifiedEvent)
 def _asset_modified(obj, event):
-	if 		INTIMedia.providedBy(obj) \
-		or	INTIDocketAsset.providedBy(obj):
-		queue_modified(ASSETS_QUEUE, single_index_job, obj)
-	if INTIMedia.providedBy(obj):
-		for transcript in getattr(obj, 'transcripts', None) or ():
-			_index_transcript(transcript, None)
+    if     INTIMedia.providedBy(obj) \
+        or INTIDocketAsset.providedBy(obj):
+        queue_modified(ASSETS_QUEUE, single_index_job, obj)
+    if INTIMedia.providedBy(obj):
+        for transcript in getattr(obj, 'transcripts', None) or ():
+            _index_transcript(transcript, None)
+
 
 @component.adapter(IPresentationAsset, IIntIdRemovedEvent)
 def _asset_removed(obj, event):
-	if 		INTIMedia.providedBy(obj) \
-		or	INTIDocketAsset.providedBy(obj):
-		queue_remove(ASSETS_QUEUE, single_unindex_job, obj=obj)
-	if INTIMedia.providedBy(obj):
-		for transcript in getattr(obj, 'transcripts', None) or ():
-			queue_remove(TRANSCRIPTS_QUEUE, single_unindex_job, obj=transcript)
+    if     INTIMedia.providedBy(obj) \
+        or INTIDocketAsset.providedBy(obj):
+        queue_remove(ASSETS_QUEUE, single_unindex_job, obj=obj)
+    if INTIMedia.providedBy(obj):
+        for transcript in getattr(obj, 'transcripts', None) or ():
+            queue_remove(TRANSCRIPTS_QUEUE, single_unindex_job, obj=transcript)
+
 
 @component.adapter(IPresentationAsset, IPresentationAssetMovedEvent)
 def _asset_moved(obj, event):
-	if INTIMedia.providedBy(obj) or INTIDocketAsset.providedBy(obj):
-		queue_modified(ASSETS_QUEUE, single_index_job, obj=obj)
+    if INTIMedia.providedBy(obj) or INTIDocketAsset.providedBy(obj):
+        queue_modified(ASSETS_QUEUE, single_index_job, obj=obj)
+
 
 @component.adapter(IPresentationAsset, IIndexObjectEvent)
 def _index_asset(obj, event):
-	_asset_added(obj, None)
+    _asset_added(obj, None)
+
 
 @component.adapter(IPresentationAsset, IUnindexObjectEvent)
 def _unindex_asset(obj, event):
-	_asset_removed(obj, None)
+    _asset_removed(obj, None)
