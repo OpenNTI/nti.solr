e1b45ecd1e33506e639c024fe4cd71f1b148ff0d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-04 18:40:33 +0000

    fix dep
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@105828 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     tests_require=TESTS_REQUIRE,
     install_requires=[
         'setuptools',
-        'BTree',
+        'BTrees',
         'nti.async',
         'nti.base',
         'nti.contentindexing',
