d6d65f66f9fa4e300120661787636f0fe1c8f495
Josh Zuech <josh.zuech@nextthought.com>
2017-03-14 22:43:04 +0000

    Try to simplify search fragment sanitation
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@108842 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/decorators.py
+++ src/nti/solr/decorators.py
@@ -11,9 +11,6 @@ logger = __import__('logging').getLogger(__name__)
 
 import re
 
-from html5lib import HTMLParser
-from html5lib import treebuilders
-
 from zope import component
 
 from nti.base._compat import to_unicode
@@ -46,62 +43,22 @@ class _SearchFragmentDecorator(object):
     @classmethod
     def sanitize(cls, raw, tag='em'):
         """
-        Clean any HTML by parsing the DOM elements getting their text 
-        and only leaving only the hightlighting element tags
+        Transform to plain text, and then make sure the items already
+        wrapped in `tag` remain wrapped in `tag`.
         """
-        try:
-            text = []
-            end_ele = '</%s>' % tag
-            start_ele = '<%s>' % tag
-            parser = HTMLParser(tree=treebuilders.getTreeBuilder("lxml"),
-                                namespaceHTMLElements=False)
-            doc = parser.parse(raw)
-            for element in doc.iter("*"):
-                if element.tag == tag:
-                    text.append(start_ele)
-                if element.text is not None:
-                    content = component.getAdapter(element.text,
-                                                   IPlainTextContentFragment,
-                                                   name='text')
-                    text.append(content)
-                if element.tag == tag:
-                    text.append(end_ele)
-            raw = ''.join(text)
-        except Exception as e:
-            logger.warn("Could not parse %s. %s", raw, e)
-        return raw
-
-    @classmethod
-    def split_and_sanitize(cls, raw, tag='em'):
-        result = []
-        prior = None
-        end_ele = '</%s>' % tag
-        start_ele = '<%s>' % tag
         # clean raw string and make it unicode
         raw = re.sub(r'\\/', '/', to_unicode(raw))
         # clean anything that may come from a valid <html> tag
         m = re.match(r"(.*)(<html>.*)", raw)
         raw = m.groups()[1] if m else raw
-        # split by enclosing element
-        for split in re.split(r'(%s)' % start_ele, raw):
-            if split != start_ele:
-                if prior == start_ele:
-                    # process highlighted element (e.g. <em>cell</em>)
-                    idx = split.find(end_ele)
-                    end_idx = idx + len(end_ele)
-                    enclosed = '%s%s' % (prior, split[:end_idx])
-                    result.append(cls.sanitize(enclosed))
-                    # process rest
-                    split = split[end_idx:]
-                    # add any extra spaces for next clean up
-                    m = re.search(r'[^\s]', split)
-                    if m and m.start() != 0:
-                        result.append(split[:m.start()])
-                        split = split[m.start():]
-                if split:
-                    result.append(cls.sanitize(split))
-            prior = split
-        return ''.join(result)
+        # Find all matches
+        matches = re.findall(r"<%s>(.*?)</%s>" % (tag, tag), raw)
+        matches = set(matches or ())
+        raw = IPlainTextContentFragment(raw)
+        # Loop and retain our wrapped matches
+        for match in matches:
+            raw = raw.replace(match, '<%s>%s</%s>' % (tag, match, tag))
+        return raw.strip()
 
     def decorateExternalObject(self, original, external):
         hit = original.__parent__
@@ -110,7 +67,7 @@ class _SearchFragmentDecorator(object):
             and not ITranscriptSearchHit.providedBy(hit) \
             and hl_removeEncodedHTML(hit.Query):
             for idx, match in enumerate(original.Matches or ()):
-                match = self.split_and_sanitize(match)
+                match = self.sanitize(match)
                 external['Matches'][idx] = match
 
 
--- src/nti/solr/tests/test_decorators.py
+++ src/nti/solr/tests/test_decorators.py
@@ -25,18 +25,24 @@ class TestDecorators(unittest.TestCase):
 
     def test_fragment_1(self):
         s = 'how our chemical messengers are synthesized and secreted from the secretory <em>cell</em>. Now'
-        assert_that(_SearchFragmentDecorator.split_and_sanitize(s), is_(s))
+        assert_that(_SearchFragmentDecorator.sanitize(s), is_(s))
 
     def test_fragment_2(self):
         s = "The function of one type of parathyroid <em>cells</em> and other <em>cells</em> in the body"
-        assert_that(_SearchFragmentDecorator.split_and_sanitize(s), is_(s))
+        assert_that(_SearchFragmentDecorator.sanitize(s), is_(s))
 
     def test_fragment_3(self):
+        # XXX: Is this a valid test case?
         s = u'the protein-digesting enzyme <em>pepsin.&lt;/span>&lt;/div>\'</em>'
-        assert_that(_SearchFragmentDecorator.split_and_sanitize(s),
+        assert_that(_SearchFragmentDecorator.sanitize(s),
                     is_(u"the protein-digesting enzyme <em>pepsin.'</em>"))
 
     def test_fragment_4(self):
         s = u'//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><head>      </head><body>The Adaptive Immune Response: T <em>Lymphocytes</em> and Their Functional'
-        assert_that(_SearchFragmentDecorator.split_and_sanitize(s),
+        assert_that(_SearchFragmentDecorator.sanitize(s),
                     is_(u"The Adaptive Immune Response: T <em>Lymphocytes</em> and Their Functional"))
+
+    def test_wowza(self):
+        hit = "<html><body>/W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html><head>         </head> <body>  realtitle <p></p><p>  realtitle </p><p></p> <h2>partitle</h2><h2>partitle</h2><h1>jz title</h1><h1>jz title</h1><a></a> <p>imaparagraph</p> <h1>jz title</h1><a></a> <p><em>wowza</em> <em>wowza</em></p> <a></a> <p><em>wowza</em></p> <a></a> <p>adlk;fj;</p> <a></a> <p>w;k</p>  </body></html>"
+        assert_that(_SearchFragmentDecorator.sanitize(hit),
+                    is_(u'realtitle   realtitle  partitlepartitlejz titlejz title imaparagraph jz title <em>wowza</em> <em>wowza</em>  <em>wowza</em>  adlk;fj;  w;k'))
