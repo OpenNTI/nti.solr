a83dcca85511958c40062ff6606f6d7150eee43b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-18 17:06:46 +0000

    use constant
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.solr/trunk@110917 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/solr/transcripts.py
+++ src/nti/solr/transcripts.py
@@ -232,6 +232,8 @@ def _transcript_to_catalog(obj):
     return component.getUtility(ICoreCatalog, name=TRANSCRIPTS_CATALOG)
 
 
+POST_FIX_SEP = u'='
+
 def _transcript_documents_creator(transcript, factory=TranscriptDocument):
     if INTITranscript.providedBy(transcript):
         result = []
@@ -242,8 +244,8 @@ def _transcript_documents_creator(transcript, factory=TranscriptDocument):
                                   provided=IMetadataDocument)
         for x, entry in enumerate(TranscriptContentValue.entries(transcript)):
             doc = factory()
-            doc.__dict__.update(source.__dict__)  # update with source
-            doc.id = "%s=%s" % (uid, x)  # = is id postfix
+            doc.__dict__.update(source.__dict__) # update with source
+            doc.id = "%s%s%s" % (uid, POST_FIX_SEP, x)  # = is id postfix
             doc.media = media  # ntiid of the media object
             doc.content_en = IContentValue(entry).value()
             doc.cue_end_time = ITranscriptCueEndTimeValue(entry).value()
@@ -286,7 +288,7 @@ class TranscriptsCatalog(MetadataCatalog):
     def build_from_search_query(self, query, **kwargs):
         term, fq, params = MetadataCatalog.build_from_search_query(self, query, **kwargs)
         packs = getattr(query, 'packages', None) \
-            or getattr(query, 'package', None)
+             or getattr(query, 'package', None)
         if 'containerId' not in fq and packs:
             packs = packs.split() if isinstance(packs, string_types) else packs
             fq.add_or('containerId', [lucene_escape(x) for x in packs])
@@ -298,6 +300,6 @@ class TranscriptsCatalog(MetadataCatalog):
     def clear(self, commit=None):
         types = self.get_mime_types(self.name)
         q = "mimeType:(%s)" % self._OR_.join(lucene_escape(x) for x in types)
-        self.client.delete(q=q,
-                           commit=self.auto_commit if commit is None else bool(commit))
+        commit = self.auto_commit if commit is None else bool(commit)
+        self.client.delete(q=q, commit=commit)
     reset = clear
