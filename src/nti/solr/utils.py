aab5d03c3161cc4da011349ab2553e586229833f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-01 17:56:15 -0500

    pylint

--- src/nti/solr/utils.py
+++ src/nti/solr/utils.py
@@ -9,11 +9,12 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import re
-import six
 import functools
 from collections import defaultdict
 
 import gevent
+
+import six
 
 from zope import component
 
