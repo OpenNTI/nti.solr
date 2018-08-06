d9afe9952d6edfe290732abcff604c9cd3362863
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-06 15:53:05 -0500

    Don't use _ for param name; clean up

--- src/nti/solr/courses/subscribers.py
+++ src/nti/solr/courses/subscribers.py
@@ -34,32 +34,32 @@ logger = __import__('logging').getLogger(__name__)
 
 
 @component.adapter(ICourseInstance, IIntIdAddedEvent)
-def _course_added(obj, _):
+def _course_added(obj, unused_event=None):
     queue_add(COURSES_QUEUE, single_index_job, obj)
 
 
 @component.adapter(ICourseInstance, IObjectModifiedEvent)
 @component.adapter(ICourseCatalogEntry, IObjectModifiedEvent)
-def _course_modified(obj, _):
+def _course_modified(obj, unused_event=None):
     course = ICourseInstance(obj)
     queue_modified(COURSES_QUEUE, single_index_job, course)
 
 
 @component.adapter(ICourseInstance, ICourseInstanceImportedEvent)
-def _course_imported(obj, _):
+def _course_imported(obj, unused_event=None):
     queue_modified(COURSES_QUEUE, single_index_job, obj)
 
 
 @component.adapter(ICourseInstance, IIntIdRemovedEvent)
-def _course_removed(obj, _):
+def _course_removed(obj, unused_event=None):
     queue_remove(COURSES_QUEUE, single_unindex_job, obj=obj)
 
 
 @component.adapter(ICourseInstance, IIndexObjectEvent)
-def _index_course(obj, _):
+def _index_course(obj, unused_event=None):
     _course_added(obj, None)
 
 
 @component.adapter(ICourseInstance, IUnindexObjectEvent)
-def _unindex_course(obj, _):
+def _unindex_course(obj, unused_event=None):
     _course_removed(obj, None)
