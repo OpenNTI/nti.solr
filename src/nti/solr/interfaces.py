649f64d89015e6d7b4f34c28e04eaec022c3db5e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-05 10:34:29 -0500

    minor

--- src/nti/solr/interfaces.py
+++ src/nti/solr/interfaces.py
@@ -8,7 +8,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-# pylint: disable=inherit-non-class
+# pylint: disable=inherit-non-class,inconsistent-mro
 
 from zope import interface
 
